89f09718a3ff0b57d6424238f2c391b876071738
Lisa <imnotlostrui@aim.com>
2017-11-07 23:05:38 -0500

    populated a tableview with the GOTData

--- GOT-StudentVersion/GOT-StudentVersion/ViewController.swift
+++ GOT-StudentVersion/GOT-StudentVersion/ViewController.swift
@@ -8,18 +8,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    var episodeArr = [GOTEpisode]()
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        episodeArr = GOTEpisode.allEpisodes
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+   //2 reqd data source methods numberOfRows and cellForRowAt
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print(episodeArr.count)
+        return episodeArr.count
     }
-
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+       let cell = tableView.dequeueReusableCell(withIdentifier: "Episode Cell", for: indexPath)
+        cell.textLabel?.text = episodeArr[indexPath.row].name
+        cell.detailTextLabel?.text = "S:" + "\(episodeArr[indexPath.row].season) " + "E:" + "\(episodeArr[indexPath.row].number)"
+        cell.imageView?.image = UIImage(named: episodeArr[indexPath.row].mediumImageID)
+        //cell.textLabel?.textAlignment = .center
+        //cell.detailTextLabel?.textAlignment = .center
+       
+        print(cell)
+        return cell
+    }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 7
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        //var sectionNum = episodeArr.
+        //print(sectionNum)
+        switch  section {
+        case 1:
+            return "Season 1"
+        default:
+            return "next"
+        }
+    }
 
 }
 
