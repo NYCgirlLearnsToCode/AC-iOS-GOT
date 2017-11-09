5c26b93a3c906af49a4edf46b0775785e449f563
Lisa <imnotlostrui@aim.com>
2017-11-09 00:00:23 -0500

    changed cells from subtitle to custom and aligned

---
+++ GOT-StudentVersion/GOT-StudentVersion/LeftAlignedTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  LeftAlignedTableViewCell.swift
+//  GOT-StudentVersion
+//
+//  Created by Lisa J on 11/8/17.
+//  Copyright © 2017 C4Q . All rights reserved.
+//
+
+import UIKit
+
+class LeftAlignedTableViewCell: UITableViewCell {
+    @IBOutlet weak var episodeImageView: UIImageView!
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+}
--- GOT-StudentVersion/GOT-StudentVersion/ViewController.swift
+++ GOT-StudentVersion/GOT-StudentVersion/ViewController.swift
@@ -10,41 +10,57 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     var episodeArr = [GOTEpisode]()
-   // var alleps = AllEp
+    // var alleps = AllEp
     @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
+        //  self.tableView.rowHeight = UITableViewAutomaticDimension
+        //  self.tableView.estimatedRowHeight = 200.0
         episodeArr = GOTEpisode.allEpisodes
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-   //2 reqd data source methods numberOfRows and cellForRowAt
+    
+    //2 reqd data source methods numberOfRows and cellForRowAt
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return episodeArr.filter{$0.season == section + 1}.count
         //print(episodeArr.count)
         //return episodeArr.count
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Episode Cell"/*indexPath.section % 2 == 0 ? "Episode Cell Left":"Episode Cell Right"*/, for: indexPath)
-        cell.textLabel?.text = episodeArr.filter{$0.season == indexPath.section + 1}[indexPath.row].name
-        cell.detailTextLabel?.text = "S:" + "\(episodeArr.filter{$0.season == indexPath.section + 1}[indexPath.row].season) " + "E:" + "\(episodeArr.filter{$0.season == indexPath.section + 1}[indexPath.row].number)"
-        cell.imageView?.image = UIImage(named: episodeArr.filter{$0.season == indexPath.section + 1}[indexPath.row].mediumImageID)
-        //cell.textLabel?.textAlignment = .center
-        //cell.detailTextLabel?.textAlignment = .center
-       
-        print(cell)
-        return cell
+        let leftAligned = indexPath.section % 2 == 0
+        let cell = tableView.dequeueReusableCell(withIdentifier:"Episode Cell", for: indexPath)
+        if leftAligned {
+            if let cell = cell as? LeftAlignedTableViewCell {
+                cell.titleLabel.text = episodeArr.filter{$0.season == indexPath.section + 1}[indexPath.row].name
+                cell.descriptionLabel.text = "S:" + "\(episodeArr.filter{$0.season == indexPath.section + 1}[indexPath.row].season) " + "E:" + "\(episodeArr.filter{$0.season == indexPath.section + 1}[indexPath.row].number)"
+                cell.episodeImageView.image = UIImage(named: episodeArr.filter{$0.season == indexPath.section + 1}[indexPath.row].mediumImageID)
+                
+                return cell
+                
+                
+            }
+        }else{
+            let cell = tableView.dequeueReusableCell(withIdentifier:"Episode Cell2", for: indexPath)
+            if let cell = cell as? RightAlignedTableViewCell {
+                cell.titleLabel2.text = episodeArr.filter{$0.season == indexPath.section + 1}[indexPath.row].name
+                print(cell.titleLabel2.text)
+                cell.descriptionLabel2.text = "S:" + "\(episodeArr.filter{$0.season == indexPath.section + 1}[indexPath.row].season) " + "E:" + "\(episodeArr.filter{$0.season == indexPath.section + 1}[indexPath.row].number)"
+                
+                cell.episode2ImageView.image = UIImage(named: episodeArr.filter{$0.season == indexPath.section + 1}[indexPath.row].mediumImageID)
+                
+                return cell
+            }
+        }
+        return UITableViewCell()
     }
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 7
-    }
-    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-//        var sectionNum =
-//        print(sectionNum)
-        return "Season \(section + 1)"
-    }
-
+            func numberOfSections(in tableView: UITableView) -> Int {
+                return 7
+            }
+            func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+                return "Season \(section + 1)"
+            }
+    
 }
 
